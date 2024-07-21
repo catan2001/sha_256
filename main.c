e62f256567bed5aade6c7487d3e21d57660a8f45
catan2001 <nikolacatic2001@gmail.com>
2024-07-21 19:24:13 +0200

    cleanup

--- main.c
+++ main.c
@@ -5,9 +5,13 @@ int main(void) {
     // TODO: implement later when string is larger than 64
     char str[] = "AlokinCitac";
     unsigned int hash[8];
-    if(sha_256(str, hash) == 0)
+    printf("Input string: %s\n", str);
+    if(sha_256(str, hash) == 0) {
+        printf("Calculated hash: \n");
         for(unsigned char i = 0; i < 8; ++i)
-            printf("%04x ", hash[i]);
+            printf("%04x", hash[i]);
+        printf("\n");
+    }
     else
         printf("ERROR: could not compute hash\n");
     return 0;
